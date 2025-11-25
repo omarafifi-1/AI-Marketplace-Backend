e0418dcb15e821a75bebf3e63586f3253468107a
Omar Afifi <omarafifi137@gmail.com>
2025-11-25 17:04:03 +0200

    Implemented Category Repo

---
+++ AI-Marketplace.Infrastructure/Repositories/Categories/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using AI_Marketplace.Application.Common.Interfaces;
+using AI_Marketplace.Domain.Entities;
+using AI_Marketplace.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Marketplace.Infrastructure.Repositories.Categories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext applicationDbContext) 
+        {
+            _context = applicationDbContext;
+        }
+
+        public async Task<Category?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Categories.FindAsync(new object[] { id }, cancellationToken);
+        }
+
+        public async Task<List<Category>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.Categories.ToListAsync(cancellationToken);
+        }
+
+        public async Task<Category> CreateAsync(Category category, CancellationToken cancellationToken = default)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync(cancellationToken);
+            return category;
+        }
+    }
+}
