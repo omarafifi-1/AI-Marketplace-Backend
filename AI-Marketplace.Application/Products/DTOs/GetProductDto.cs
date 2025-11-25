2f8d3d5cb1a20764ee1938290460c26105595ae0
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2025-11-25 20:21:10 +0200

    Added some data in the seeder, added property in productDto

--- AI-Marketplace.Application/Products/DTOs/GetProductDto.cs
+++ AI-Marketplace.Application/Products/DTOs/GetProductDto.cs
@@ -10,6 +10,8 @@ namespace AI_Marketplace.Application.Products.DTOs
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
         public int Stock { get; set; }
         public int CategoryId { get; set; }
         public DateTime CreatedAt { get; set; }
