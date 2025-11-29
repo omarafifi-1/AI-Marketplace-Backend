8b4ec3d5be51b27ae8ef149e65d4a1afbc9c750a
Omar Afifi <omarafifi137@gmail.com>
2025-11-29 18:05:25 +0200

    Created Orders structure, repo and required migration

---
+++ AI-Marketplace.Application/Common/Interfaces/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using AI_Marketplace.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Marketplace.Application.Common.Interfaces
+{
+    public interface IOrderRepository
+    {
+        public Task<List<Order>> GetAllOrdersAsync(CancellationToken cancellationToken);
+        public Task<Order?> GetOrderByIdAsync(int id, CancellationToken cancellationToken);
+        public Task<List<Order>> GetOrdersByStoreId(int storeId, CancellationToken cancellationToken);
+        public Task<Order> ChangeOrderStatusAsync(int id, string status, CancellationToken cancellationToken);
+        
+    }
+}
---
+++ AI-Marketplace.Infrastructure/Repositories/Orders/OrderRepository.cs
@@ -0,0 +1,74 @@
+﻿using AI_Marketplace.Application.Common.Exceptions;
+using AI_Marketplace.Application.Common.Interfaces;
+using AI_Marketplace.Domain.Entities;
+using AI_Marketplace.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Marketplace.Infrastructure.Repositories.Orders
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderRepository(ApplicationDbContext applicationDbContext) 
+        {
+            _context = applicationDbContext;
+        }
+
+        public Task<List<Order>> GetAllOrdersAsync(CancellationToken cancellationToken)
+        {
+            return _context.Orders
+                .Include(o => o.Buyer)
+                .Include(o => o.Store)
+                .Include(o => o.Offer)
+                .Include(o => o.OrderItems)
+                .ThenInclude(oi => oi.Product)
+                .ToListAsync(cancellationToken);
+        }
+
+        public Task<Order?> GetOrderByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            return _context.Orders
+                .Include(o => o.Buyer)
+                .Include(o => o.Store)
+                .Include(o => o.Offer)
+                .Include(o => o.OrderItems)
+                .ThenInclude(oi => oi.Product)
+                .FirstOrDefaultAsync(o => o.Id == id, cancellationToken);
+        }
+
+        public async Task<List<Order>> GetOrdersByStoreId(int storeId, CancellationToken cancellationToken)
+        {
+            return await _context.Orders
+                .Include(o => o.Buyer)
+                .Include(o => o.Store)
+                .Include(o => o.Offer)
+                .Include(o => o.OrderItems)
+                .ThenInclude(oi => oi.Product)
+                .Where(o => o.StoreId == storeId)
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<Order> ChangeOrderStatusAsync(int id, string status, CancellationToken cancellationToken)
+        {
+            var order = await _context.Orders.FindAsync(new object[] { id }, cancellationToken);
+            
+            if (order == null)
+                throw new NotFoundException(new Dictionary<string, string[]>
+                {
+                    { "Order", new[] { $"Order With Id {id} Not Found." } }
+                });
+            
+            order.Status = status;
+            
+            if (status == "Delivered")
+                order.DeliveredAt = DateTime.UtcNow;
+            
+            await _context.SaveChangesAsync(cancellationToken);
+            return order;
+        }
+    }
+}
