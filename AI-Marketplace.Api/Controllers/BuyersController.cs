e95d0f167ff814775822c7c750bb5c5571ed5a96
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2025-11-30 17:46:59 +0200

    Built get all buyer orders, get buyer order by id endpoints

---
+++ AI-Marketplace.Api/Controllers/BuyersController.cs
@@ -0,0 +1,69 @@
+﻿using AI_Marketplace.Application.Buyers.Queries.GetAllBuyerOrders;
+using AI_Marketplace.Application.Buyers.Queries.GetBuyerOrderById;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AI_Marketplace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Customer")]
+    public class BuyersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BuyersController(IMediator mediator)
+        {
+            _mediator = mediator;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders([FromQuery] string? status = "all")
+        {
+            var UserIdString = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (UserIdString == null)
+            {
+                return Unauthorized();
+            }
+            var UserIdInt = int.Parse(UserIdString);
+            if (!int.TryParse(UserIdString, out UserIdInt))
+            {
+                return BadRequest("Invalid User ID");
+            }
+
+            var query = new GetAllBuyerOrderQuery
+            {
+                BuyerId = UserIdInt,
+                Status = status
+            };
+
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOrderById(int id)
+        {
+            var UserIdString = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (UserIdString == null)
+            {
+                return Unauthorized();
+            }
+            var UserIdInt = int.Parse(UserIdString);
+            if (!int.TryParse(UserIdString, out UserIdInt))
+            {
+                return BadRequest("Invalid User ID");
+            }
+
+            var query = new GetBuyerOrderByIdQuery(id, UserIdInt);
+            var result = await _mediator.Send(query);
+
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+    }
+}
