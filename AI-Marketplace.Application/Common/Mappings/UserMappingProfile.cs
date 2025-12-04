1b14d33ebc08cfe20656bc316d58aa65b74cb352
Ahmed Mokhtar <ahmedmohktar26@gmail.com>
2025-12-04 11:30:18 +0200

    Build GET/PUT /api/users/profile endpoints

---
+++ AI-Marketplace.Application/Common/Mappings/UserMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AI_Marketplace.Application.Users.DTOs;
+using AI_Marketplace.Domain.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Marketplace.Application.Common.Mappings
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<ApplicationUser, GetUserProfileDto>();
+
+            CreateMap<UpdateUserProfileDto, ApplicationUser>()
+          .ForMember(dest => dest.Id, opt => opt.Ignore())
+          .ForMember(dest => dest.Email, opt => opt.Ignore())
+          .ForMember(dest => dest.UserName, opt => opt.Ignore())
+          .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+          .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+          .ForMember(dest => dest.LastModifiedAt, opt => opt.MapFrom(_ => DateTime.UtcNow));
+        }
+    }
+}
---
+++ AI-Marketplace.Application/Users/Queries/GetUserProfile/GetUserProfileQuery.cs
@@ -0,0 +1,17 @@
+﻿using AI_Marketplace.Application.Users.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Marketplace.Application.Users.Queries.GetUserProfile
+{
+    public class GetUserProfileQuery : IRequest<GetUserProfileDto>
+    {
+        public int UserId { get; set; }
+        public GetUserProfileQuery(int userId)
+        {
+            UserId = userId;
+        }
+    }
+}
